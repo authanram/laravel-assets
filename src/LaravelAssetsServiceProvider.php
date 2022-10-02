042a8922947c94526aeed767177e4b76e103be72
Daniel Seuffer <authanram@gmail.com>
2022-10-02 21:36:44 +0200

    WIP

--- src/LaravelAssetsServiceProvider.php
+++ src/LaravelAssetsServiceProvider.php
@@ -10,16 +10,10 @@ class LaravelAssetsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-assets')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_laravel-assets_table')
             ->hasCommand(LaravelAssetsCommand::class);
     }
 }
